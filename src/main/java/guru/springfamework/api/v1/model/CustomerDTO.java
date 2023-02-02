95580520c30a24c89c5c6c5bf9fa6b9930a17c77
Yakodelaragua <araycdd@gmail.com>
2023-02-02 10:39:00 +0100

    customer service

---
+++ src/main/java/guru/springfamework/api/v1/model/CustomerDTO.java
@@ -0,0 +1,10 @@
+package guru.springfamework.api.v1.model;
+
+import lombok.Data;
+
+@Data
+public class CustomerDTO {
+    private String firtsname;
+    private String lastname;
+    private String customerUrl;
+}
